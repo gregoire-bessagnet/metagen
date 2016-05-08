dbfaed9e4c2c8740094a5724582718c349a9ca05
Gregoire <mygreg@inbox.com>
2016-05-08 17:04:54 +0200

    scaffold inferno

--- metagen/db/schema.rb
+++ metagen/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508144107) do
+ActiveRecord::Schema.define(version: 20160508145239) do
+
+  create_table "ideas", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "picture"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "wordone", force: :cascade do |t|
     t.string "word_one"
